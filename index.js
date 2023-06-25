b2298b2903d6fd87aa94b2460648d8222308fd20
Rbr4t <robert.koor@opilased.nrg.edu.ee>
2023-06-25 18:29:57 +0300

    some progress

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ import Tree from "./tree.js";
 
 const newTree = new Tree();
 
-newTree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+newTree.buildTree([2, 1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 64, 62, 65, 6345, 324])
 
 // prettyprint(newTree.root);
 
 newTree.insert(69)
 
-// prettyprint(newTree.root)
+prettyprint(newTree.root)
 
-newTree.delete(69)
+// newTree.delete(69)
+// newTree.delete(67)
+// newTree.delete(67)
+console.log(newTree.depth(69))
 
 // prettyprint(newTree.root)
--- tree.js
+++ tree.js
@@ -6,33 +6,25 @@ export default class Tree {
         this.root = null
     }
 
-    _traverse(element, withparent = false) {
+    _traverseForBuilding(element) {
         if(this.root === null){
             
             this.root = new Node(element);
-            if(withparent){
-              return (this.root, this.root)
-            }
             return this.root
 
         } else {
             // traverse the tree
             let currentNode = this.root;
-            let parentNode = currentNode;
             
             while (currentNode.left !== null || currentNode.right !== null) {
                 if (element >= currentNode.value) {
                   if (currentNode.right !== null) {
-                    parentNode = currentNode
-
                     currentNode = currentNode.right;
                   } else {
                     break;
                   }
                 } else {
                   if (currentNode.left !== null) {
-                    parentNode = currentNode
-
                     currentNode = currentNode.left;
                   } else {
                     break;
@@ -40,19 +32,13 @@ export default class Tree {
 
                 }
 
-                
               }
-
-        if(withparent){
-          return [currentNode, parentNode]
-        }
         return currentNode
         
             }
     }
 
     _addNode(element, currentNode) {
-        // console.log(element)
         if(element >= currentNode.value) {
             currentNode.right = new Node(element)
         } else {
@@ -61,7 +47,7 @@ export default class Tree {
     }
 
     buildTree(array) {
-        const sorted_array = array
+        const sorted_array = [...new Set(array)]
         // Sort the array first and remove the duplicates -- don't
 
         // construct the tree using Node objects
@@ -69,7 +55,7 @@ export default class Tree {
             if (this.root === null) {
               this.root = new Node(element);
             } else {
-              let currentNode = this._traverse(element);
+              let currentNode = this._traverseForBuilding(element);
               this._addNode(element, currentNode);
             }
           });
@@ -79,12 +65,144 @@ export default class Tree {
 
     insert(number) {
         // traverse the tree and add the number to the tree
-        let currentNode = this._traverse(number);
+        let currentNode = this._traverseForBuilding(number);
         this._addNode(number, currentNode);
     };
 
-    delete(number) {
-      console.log(this._traverse(number, true))
-      
+    _traverseForDeleting(node, number, parent=node) {
+      if(node===null) {
+        return -1
+      }
+      if(node.value > number) {
+        return this._traverseForDeleting(node.left, number, node)
+      } else if(node.value < number) {
+        return this._traverseForDeleting(node.right, number, node)
+      } else {
+        return [node, parent]
+      }
     }
+
+
+    // using the Breadth-Search-Method
+    _findMinimum(nodes) {
+      let queue = [];
+      let minimum = nodes;
+      queue.push(nodes)
+
+      while(queue.length > 0) {
+        const current = queue.shift()
+        // first we check the node's value
+        if (current.value < minimum.value) {
+          minimum = current;
+        }
+
+        // and we continue traversing
+        if(current.left !== null) {
+          queue.push(current.left)
+        } 
+        if(current.right !== null) {
+          queue.push(current.right)
+        } 
+      }
+      
+      return minimum
+    }
+
+    delete(number) {
+      // traverse the tree to the node which we want to delete
+      const nodes = this._traverseForDeleting(this.root, number)
+      // save a link to its parent
+      const node = nodes[0];
+      let parent = nodes[1];
+
+      // delete the node
+      // 1.st if it has no children, no need to do fancy stuff, just get rid of the reference to its parent
+      if(node.left === null && node.right === null) {
+        if(parent.left === node) {
+          parent.left = null
+        } else if(parent.right === node) {
+          parent.right = null
+        }
+      } 
+      // 2nd if it has 1 child node, connect its child(s) and its parent
+      else if((node.left !== null || node.right !== null) && !(node.left !==null && node.right!==null)) {
+        if(parent.left === node) {
+          if(node.left === null) {
+            parent.left = node.right
+          } else {
+            parent.left = node.left
+          } 
+        } else if(parent.right === node) {
+          if(node.left === null) {
+            parent.right = node.right
+          } else {
+            parent.right = node.left
+          } 
+        }
+      } 
+      // 3rd if it has 2 child nodes, set right side tree's minimum value the in place of the deleted node
+  
+      else {
+        // we have a minimum node
+        let minimum_node = { ...this._findMinimum(node.right)}
+        minimum_node.left = node.left
+        minimum_node.right = node.right
+        // switch to-be-deleted node and minimum node
+        
+        if(parent.left === node) {
+          parent.left = minimum_node
+        } else if(parent.right === node) {
+          parent.right = minimum_node
+        }
+
+        // delete the copy
+        const parentOfNewNode = this._traverseForDeleting(minimum_node.right, minimum_node.value, true)
+        if(parentOfNewNode[1].left.value === minimum_node.value ) {
+          parentOfNewNode[1].left = null
+        } else if(parentOfNewNode[1].right.value === minimum_node.value) {
+          parentOfNewNode[1].right = null
+
+        }
+      }
+
+    }
+
+    find(number) {
+      return(this._traverseForDeleting(this.root, number)[0]) 
+    }
+
+    // was supposed to be BFS, but I have it essentially already done in the _findMinimum method
+    levelOrder(callback) {
+
+    }
+
+    // node -> root
+    depth(number, node=this.root) {
+      if(node.left===null || node.right===null){
+        return 0
+      } else if(node.value === number) {
+        return 0
+      } else {
+        return this.depth(number, node.left) + this.depth(number, node.right) + 1
+      }
+    }
+
+    // node -> leaves
+    height(number, node =this.root) {
+      function getTotalHeight(node) {
+        if(node===null) {
+          return 0
+        }
+        let left = getTotalHeight(node.left)
+        let right = getTotalHeight(node.right)
+        if(left>right) {
+          return left + 1
+        } else {
+          return right + 1
+        }
+
+      }
+      console.log(this.depth(7))
+    }
+
   }
